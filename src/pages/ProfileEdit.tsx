3ea33fe1343303784f9258dfc6c676c40971aba0
AhmedAtri <ahmed.atri@isimsf.u-sfax.tn>
2024-07-14 20:26:18 +0100

    started edit profile

--- src/pages/ProfileEdit.tsx
+++ src/pages/ProfileEdit.tsx
@@ -1,14 +1,39 @@
 import React, { useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonCard, IonCardContent, IonImg, IonText, IonIcon, IonButtons } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonCard, IonCardContent, IonImg, IonText, IonIcon, IonButtons, IonInput } from '@ionic/react';
 import { menu } from 'ionicons/icons';
 import UserSidebar from '../components/layouts/user/UserSidebar';
 import { useAuthContext } from '../context/AuthProvider';
 import User from '../models/User';
 import Loading from '../components/Loading';
+import axios from 'axios';
 
 const ProfileEdit: React.FC = () => {
     const { user }: { user: User } = useAuthContext();
     const [isExpanded, setIsExpanded] = useState(false);
+
+    const [error, setError] = useState<string>();
+
+  const [loginInput, setLogin] = useState({
+    fname: '',
+    lname: '',
+    email:'',
+    password: '',
+    passwordConfirmation:'',
+    oldPassword:''
+  })
+
+  const handleInput = (e: any) => {
+    const target = e.target as HTMLInputElement;
+    setLogin({ ...loginInput, [target.name]: e.detail.value })
+  }
+
+  const handleSubmit =(e:any)=>{
+    axios.put(`/profile`, loginInput).then((res=>{
+        console.log("success")
+    })).catch((err:any)=>{
+        console.log(err)
+    })
+  }
 
     if (!user) return <Loading type='page' />
 
@@ -31,9 +56,13 @@ const ProfileEdit: React.FC = () => {
                     <IonCard>
                         <IonCardContent>
                             <IonImg className='' src='https://i.pinimg.com/736x/8b/16/7a/8b167af653c2399dd93b952a48740620.jpg'></IonImg>
-                            <IonText className="text-black text-xl font-bold flex justify-center">{user?.lname + " " + user?.fname}</IonText>
-                            <IonText className="text-lg flex justify-center">@{user?.username}</IonText>
-                            <div className='flex justify-center'><IonButton className='blue'>Editer les infos du compte</IonButton></div>
+                            <IonInput type='text' label="Prenom" onIonInput={handleInput} value={loginInput.fname} name='fname' labelPlacement="floating" />
+                            <IonInput type='text' label="Nom" onIonInput={handleInput} value={loginInput.lname} name='lname' labelPlacement="floating" />
+                            <IonInput type='email' label="Email" onIonInput={handleInput} value={loginInput.email} name='email' labelPlacement="floating" />
+                            <IonInput type='password' label="Mot de passe" onIonInput={handleInput} value={loginInput.password} name='password' labelPlacement="floating" />
+                            <IonInput type='password' label="Confirmer le Mot de passe" onIonInput={handleInput} value={loginInput.passwordConfirmation} name='passwordConfirmation' labelPlacement="floating" />
+                            <IonInput type='password' label="Ancien Mot de passe" onIonInput={handleInput} value={loginInput.oldPassword} name='oldPassword' labelPlacement="floating" />
+                            <div className='flex justify-center'><IonButton className='blue' onClick={handleSubmit}>Mise a jour</IonButton></div>
                         </IonCardContent>
                     </IonCard>
                 </div>
